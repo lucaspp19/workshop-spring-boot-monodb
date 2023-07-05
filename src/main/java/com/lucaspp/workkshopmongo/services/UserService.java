ca195c2deeadcc4c91830f80cbdea53c368655cb
lucaspp19 <lucaspp191@gmail.com>
2023-07-05 17:57:55 -0300

    Obtendo um usuário por id

--- src/main/java/com/lucaspp/workkshopmongo/services/UserService.java
+++ src/main/java/com/lucaspp/workkshopmongo/services/UserService.java
@@ -1,22 +1,31 @@
 package com.lucaspp.workkshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lucaspp.workkshopmongo.entities.User;
 import com.lucaspp.workkshopmongo.repository.UserRepository;
+import com.lucaspp.workkshopmongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<User> findAll() {
-		
+
 		return repository.findAll();
+	}
+
+	public User findById(String id) {
+
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+
 	}
 
 }
