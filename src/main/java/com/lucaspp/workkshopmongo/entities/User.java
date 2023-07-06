541c52ce2144aa2c0dd9afbe93c169684417ac4c
lucaspp19 <lucaspp191@gmail.com>
2023-07-06 11:13:23 -0300

    Referenciando os posts do usuário

--- src/main/java/com/lucaspp/workkshopmongo/entities/User.java
+++ src/main/java/com/lucaspp/workkshopmongo/entities/User.java
@@ -1,23 +1,29 @@
 package com.lucaspp.workkshopmongo.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document (collection = "user")
+@Document(collection = "user")
 public class User implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private String id;
 	private String name;
 	private String email;
-	
+
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
+
 	public User() {
-		
+
 	}
 
 	public User(String id, String name, String email) {
@@ -56,6 +62,14 @@ public class User implements Serializable {
 		return Objects.hash(id);
 	}
 
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -67,10 +81,5 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-
-	
-	
-	
-	
 
 }
