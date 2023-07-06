28df20772d82ffacae9e8924117f3a95ff0c298e
lucaspp19 <lucaspp191@gmail.com>
2023-07-06 11:20:05 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/lucaspp/workkshopmongo/resource/UserResource.java
+++ src/main/java/com/lucaspp/workkshopmongo/resource/UserResource.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.lucaspp.workkshopmongo.dto.UserDTO;
+import com.lucaspp.workkshopmongo.entities.Post;
 import com.lucaspp.workkshopmongo.entities.User;
 import com.lucaspp.workkshopmongo.services.UserService;
 
@@ -32,48 +33,50 @@ public class UserResource {
 		return ResponseEntity.ok().body(listDto);
 
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<UserDTO> findById(@PathVariable String id){
-		
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
-		
-		
+
 	}
+
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
-		
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+
 		User obj = service.fromDTO(objDto);
 		obj = service.insert(obj);
-		URI uri = ServletUriComponentsBuilder
-				.fromCurrentRequest()
-				.path("/{id}")
-				.buildAndExpand(obj.getId())
-				.toUri();
-		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
 		return ResponseEntity.created(uri).build();
-		
-		
+
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	public ResponseEntity<Void> delete(@PathVariable String id){
-		
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+
 		service.delete(id);
 		return ResponseEntity.noContent().build();
-		
-		
+
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id){
-		
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+
 		User obj = service.fromDTO(objDto);
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
-		
+
+	}
+
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+
 	}
 
 }
