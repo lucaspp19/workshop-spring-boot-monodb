48ba2e280ed86562839ca4efee60321ee6f3c5d2
lucaspp19 <lucaspp191@gmail.com>
2023-07-06 11:28:07 -0300

    Obtendo um post por id

---
+++ src/main/java/com/lucaspp/workkshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.lucaspp.workkshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucaspp.workkshopmongo.entities.Post;
+import com.lucaspp.workkshopmongo.repository.PostRepository;
+import com.lucaspp.workkshopmongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+
+	public Post findById(String id) {
+
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+
+	}
+
+}
